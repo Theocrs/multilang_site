bc8a7a0070d864ce15eff8c095d372ac5c295651
Theocrs <129271633+Theocrs@users.noreply.github.com>
2024-06-28 12:31:37 +0200

    Update settings.py

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -129,7 +129,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale/')]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
