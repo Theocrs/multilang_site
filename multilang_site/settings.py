16fd033f5bbb23d98aba807ff9533d0845114ec7
Theocrs <129271633+Theocrs@users.noreply.github.com>
2024-06-28 13:38:42 +0200

    Update settings.py

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -130,6 +130,9 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale/')]
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 
 # Default primary key field type
